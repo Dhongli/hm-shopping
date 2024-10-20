fe318991f92d6da6f50e60b11f50ff5beff56f18
Dahulu <1215765236@qq.com>
2024-10-20 22:12:19 +0800

    搜索页实现

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,5 @@
 const INFO_KEY = 'hm_shopping_info'
+const HISTORY_KEY = 'hm_history_list'
 
 // 本地获取客户信息
 export const getInfo = () => {
@@ -19,4 +20,15 @@ export const setInfo = (info) => {
 // 本地清除客户信息
 export const removeInfo = () => {
   localStorage.removeItem(INFO_KEY)
+}
+
+// 获取搜索历史
+export const getHistoryList = () => {
+  const result = localStorage.getItem(HISTORY_KEY)
+  return result ? JSON.parse(result) : []
+}
+
+// 设置搜索历史
+export const setHistoryList = (arr) => {
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(arr))
 }
